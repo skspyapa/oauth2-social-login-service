cc82f0e40861e2afe3426b067eded309d5967811
skspyapa <supun@rumex.lk>
2021-07-05 23:37:38 +0530

    Rename main class

--- src/main/java/lk/sky360solutions/authentication/Application.java
+++ src/main/java/lk/sky360solutions/authentication/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oauth2SocialLoginServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Oauth2SocialLoginServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
