ad863c96adeae6414d6e6ac6a2949f76ac97c0d9
skspyapa <supun@rumex.lk>
2021-07-05 23:34:50 +0530

    Add spring initializer project

---
+++ src/main/java/lk/sky360solutions/authentication/Oauth2SocialLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.sky360solutions.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SocialLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SocialLoginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/sky360solutions/authentication/Oauth2SocialLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sky360solutions.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocialLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
